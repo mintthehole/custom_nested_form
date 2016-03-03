8d9879441a494f7a7449955dfdfab555e8e732d0
John <john.pollo@strata.co.in>
2016-03-03 15:55:21 +0530

    Added Readme for custom nested forms

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-Custom nESTED fORM
-Extenstion of Nested Form
+## Custom Nested Form
+###### Extenstion of Nested Form
+
+Ever Wondered how to make use of nested forms mor selectize.js 
+
+Here is an example for multiple select and links as well.
+
+Custom Nested Form Override Js
+```haml
+  = f.fields_for :association, :class => "hide" do |c|
+    .custom_fields
+      = c.text_field :field_name, :id => "val_new_association"
+```
+Replace assocation with actual assocation name and field name with actual field name
+
+For Multiple Select with selectize
+```haml
+	= select_tag :some_field, options_for_select([]), :multiple => :multiple,"data-association" => :association, "data-blueprint-id" => :association_fields_blueprint, :class => "custom_field_for_multiple_select cselect"
+```
+
+Remove
+add this as class
+remove_custom_nested_fields_mselect
+
+
+For link / select
+```haml
+	= link_to "link", "#", "data-association" => :association, "data-blueprint-id" => :association_fields_blueprint, :class => "add_custom_nested_fields","data-value" => value u want to set
+
+Remove
+add this as class
+remove_custom_nested_fields
--- vendor/assets/javascripts/jquery_nested_form.js
+++ vendor/assets/javascripts/jquery_nested_form.js
@@ -1,10 +1,12 @@
 (function($) {
-  window.NestedFormEvents = function() {
+  window.CustomNestedFormEvents = function() {
+    this.addCustomFields = $.proxy(this.addCustomFields, this);
     this.addFields = $.proxy(this.addFields, this);
     this.removeFields = $.proxy(this.removeFields, this);
+    this.removeCustomFields = $.proxy(this.removeCustomFields, this);
   };
 
-  NestedFormEvents.prototype = {
+  CustomNestedFormEvents.prototype = {
     addFields: function(e) {
       // Setup
       var link      = e.currentTarget;
@@ -49,6 +51,57 @@
         .trigger({ type: 'nested:fieldAdded:' + assoc, field: field });
       return false;
     },
+    addCustomFields: function(e) {
+      // Setup
+      var link      = e.currentTarget;
+
+      var assoc     = $(link).data('association');                // Name of child
+      var blueprint = $('#' + $(link).data('blueprint-id'));
+      var content   = blueprint.data('blueprint');                // Fields template
+
+      // Make the context correct by replacing <parents> with the generated ID
+      // of each of the parent objects
+      var context = ($(link).closest('.fields').closestChild('input, textarea, select').eq(0).attr('name') || '').replace(/\[[a-z_]+\]$/, '');
+      // If the parent has no inputs we need to strip off the last pair
+      var current = content.match(new RegExp('\\[([a-z_]+)\\]\\[new_' + assoc + '\\]'));
+      if (current) {
+        context = context.replace(new RegExp('\\[' + current[1] + '\\]\\[(new_)?\\d+\\]$'), '');
+      }
+
+      // context will be something like this for a brand new form:
+      // project[tasks_attributes][1255929127459][assignments_attributes][1255929128105]
+      // or for an edit form:
+      // project[tasks_attributes][0][assignments_attributes][1]
+      if (context) {
+        var parentNames = context.match(/[a-z_]+_attributes(?=\]\[(new_)?\d+\])/g) || [];
+        var parentIds   = context.match(/[0-9]+/g) || [];
+
+        for(var i = 0; i < parentNames.length; i++) {
+          if(parentIds[i]) {
+            content = content.replace(
+              new RegExp('(_' + parentNames[i] + ')_.+?_', 'g'),
+              '$1_' + parentIds[i] + '_');
+
+            content = content.replace(
+              new RegExp('(\\[' + parentNames[i] + '\\])\\[.+?\\]', 'g'),
+              '$1[' + parentIds[i] + ']');
+          }
+        }
+      }
+
+      // Make a unique ID for the new child
+      var regexp  = new RegExp('new_' + assoc, 'g');
+      var new_id  = this.newId();
+      content     = $.trim(content.replace(regexp, new_id));
+
+      var field = this.insertFields(content, assoc, link);
+      // bubble up event upto document (through form)
+
+      field
+        .trigger({ type: 'nested:fieldAdded', field: field })
+        .trigger({ type: 'nested:fieldAdded:' + assoc, field: field });
+      return new_id;
+    },
     newId: function() {
       return new Date().getTime();
     },
@@ -60,27 +113,20 @@
         return $(content).insertBefore(link);
       }
     },
-    removeFields: function(e) {
-      var $link = $(e.currentTarget),
-          assoc = $link.data('association'); // Name of child to be removed
-      
-      var hiddenField = $link.prev('input[type=hidden]');
+    removeFields: function(id) {
+      var hiddenField = $('#'+id).prev('input[type=hidden]');
       hiddenField.val('1');
-      
-      var field = $link.closest('.fields');
-      field.hide();
-      
-      field
-        .trigger({ type: 'nested:fieldRemoved', field: field })
-        .trigger({ type: 'nested:fieldRemoved:' + assoc, field: field });
+      return false;
+    },
+    removeCustomFields: function(id) {
+      var hiddenField = $("[id$='"+id+"']");
+      hiddenField.val('1');
       return false;
     }
   };
 
-  window.nestedFormEvents = new NestedFormEvents();
-  $(document)
-    .delegate('form a.add_nested_fields',    'click', nestedFormEvents.addFields)
-    .delegate('form a.remove_nested_fields', 'click', nestedFormEvents.removeFields);
+  window.CustomNestedFormEvents = new CustomNestedFormEvents();
+
 })(jQuery);
 
 // http://plugins.jquery.com/project/closestChild
@@ -91,6 +137,8 @@
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
  */
+
+
 (function($) {
         $.fn.closestChild = function(selector) {
                 // breadth first search for the first matched node
@@ -112,3 +160,61 @@
                 return $();//nothing found
         };
 })(jQuery);
+
+$(document).ready(function() {
+  $(".add_custom_nested_fields").live("click",function(event){
+    event.preventDefault();
+    new_id =CustomNestedFormEvents.addCustomFields(event);
+    $(this).attr('generated_id',new_id);
+    $(this).removeClass('add_custom_nested_fields');
+    $(this).addClass('remove_custom_nested_fields');
+    $('#val_'+new_id).val($(this).data('value'));
+    if ($('#entity_'+new_id)){
+      $('#entity_'+new_id).val($(this).data('entity'));
+    }
+  });
+
+  $(".remove_custom_nested_fields").live("click",function(event){
+    event.preventDefault();
+    CustomNestedFormEvents.removeFields($(this).attr('generated_id'));
+    $(this).addClass('add_custom_nested_fields');
+    $(this).removeClass('remove_custom_nested_fields');
+    $(this).removeAttr('generated_id');
+    return false;
+  });
+
+  $(".custom_field_for_multiple_select").live("change",function(event,params){
+    event.preventDefault();
+    if (params.selected){
+      new_id = CustomNestedFormEvents.addCustomFields(event);
+      var added_id = $(".chosen-choices > .search-choice").last()[0].id
+      var length = $(this).val().length;
+      var added_value = $(this).val()[length - 1];
+      $("#"+added_id).find(".search-choice-close").attr("generated_id",new_id);
+      $("#"+added_id).find(".search-choice-close").addClass("remove_custom_nested_fields_mselect");
+      $('#val_'+new_id).val(added_value);
+    }
+    return false;
+  });
+
+  $(".search-choice-close").live("click",function(event){
+    var li = $(this).closest('li');
+    var no = $(li).index();
+    var association = $(this).attr("data-association");
+    if (association != 'undefined'){
+      var id = association+"_attributes_"+no+"__destroy";
+      CustomNestedFormEvents.removeCustomFields(id);
+    }
+    return false;
+  });
+
+  $(".remove_custom_nested_fields_mselect").live("click",function(event){
+    CustomNestedFormEvents.removeFields($(this).attr('generated_id'));
+    return false;
+  });
+
+  $(".remove_custom_nested_fields_link").live("click",function(event){
+    CustomNestedFormEvents.removeFields($(this).attr('generated_id'));
+    return false;
+  });
+});
